5eb11f537f5e923ff64bf447899d3e9336f4b847
DracoR22 <rdraco039@gmail.com>
2024-05-27 18:02:08 -0500

    schema gql added

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { DocumentBuilder } from '@nestjs/swagger'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -8,14 +8,22 @@ async function bootstrap() {
   app.enableCors()
 
   // SWAGGER API
-  const config = new DocumentBuilder().setTitle('Park Hunt | DracoR22').setDescription(
-    `The Park Hunt API.
+  const config = new DocumentBuilder()
+    .setTitle('Park Hunt | DracoR22')
+    .setDescription(
+      `The Park Hunt API.
     <h2>Looking for the graphql api?</h2>
     Go to <a href="/graphql" target="_blank">/graphql</a>.
      Or,
     You might also need to use the <a target="_blank" href="https://studio.apollographql.com/sandbox/explorer?endpoint=http://localhost:3000/graphql&document=query users{users{ uid }}
     ">Apollo explorer</a> for a greater experience.`,
-  )
+    )
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/', app, document)
 
   await app.listen(3000)
 }
